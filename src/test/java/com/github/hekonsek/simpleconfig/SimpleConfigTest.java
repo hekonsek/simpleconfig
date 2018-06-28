ca8b54eaac5f2f832075c7bf2a107a43e996a7c8
Henryk Konsek <hekonsek@gmail.com>
2018-06-28 14:50:22 +0200

    Improved tests coverage.

--- src/test/java/com/github/hekonsek/simpleconfig/SimpleConfigTest.java
+++ src/test/java/com/github/hekonsek/simpleconfig/SimpleConfigTest.java
@@ -7,16 +7,42 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SimpleConfigTest {
 
+    // Absent properties
+
+    @Test
+    public void shouldUseDefaultValueForAbsentProperty() {
+        String value = simple().config().getString("NO_SUCH_PROPERTY", "default");
+        assertThat(value).isEqualTo("default");
+    }
+
+    @Test
+    public void shouldReturnNullForAbsentProperty() {
+        String value = simple().config().getString("NO_SUCH_PROPERTY");
+        assertThat(value).isNull();
+    }
+
+    // In-memory properties
+
+    @Test
+    public void shouldReadInMemoryProperty() {
+        simple().reset().property("foo", "bar");
+        String value = simple().config().getString("foo");
+        assertThat(value).isEqualTo("bar");
+    }
+
+    @Test
+    public void shouldResetInMemoryProperties() {
+        simple().property("foo", "bar").reset();
+        String value = simple().config().getString("foo");
+        assertThat(value).isNull();
+    }
+
+    // Env properties
+
     @Test
     public void shouldResolveEnvProperty() {
         String pathValue = simple().config().getString("PATH");
         assertThat(pathValue).isNotBlank();
-    }
-
-    @Test
-    public void shouldUseDefaultValueForAbsentEnv() {
-        String value = simple().config().getString("NO_SUCH_ENV", "default");
-        assertThat(value).isEqualTo("default");
     }
 
 }
